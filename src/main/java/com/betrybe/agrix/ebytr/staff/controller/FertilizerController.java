3d744f2d1824d28e59944b4737df41e0d19b7895
Rodrigo Christofani <christofanir@gmail.com>
2024-09-10 06:15:10 -0300

    Resolução requisito 6, projeto finalizado.

--- src/main/java/com/betrybe/agrix/ebytr/staff/controller/FertilizerController.java
+++ src/main/java/com/betrybe/agrix/ebytr/staff/controller/FertilizerController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,6 +58,7 @@ public class FertilizerController {
    * @return the all fertilizers
    */
   @GetMapping
+  @Secured("ADMIN")
   public List<FertilizerDto> getAllFertilizers() {
     List<Fertilizer> allFertilizers = fertilizerService.findAll();
 
